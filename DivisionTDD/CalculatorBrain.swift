b6ef8c0dbc3ed29c885aa8465b6da40a1c7be69b
Godfrey Nolan <godfrey@riis.com>
2017-06-24 11:27:33 -0400

    first commit

---
+++ DivisionTDD/CalculatorBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.swift
+//  DivisionTDD
+//
+//  Created by User on 6/24/17.
+//  Copyright © 2017 riis. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorBrain: NSObject {
+
+}
